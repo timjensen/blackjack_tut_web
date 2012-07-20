b3610a673d642b30fdb1be0d58721856409c2a43
Tim Jensen <timdavidjensen@gmail.com>
2012-07-20 13:02:51 +1200

    made a start

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+  
+  def setup
+  end
+  
+  def user
+  end
+  
+  def user_test
+  end
+  
+  def signup_login
+  end
+  
+  def card_hand
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackWeb::Application.config.secret_token = 'd23b50d45e9c423d35e8225896641f09e204744a795be3cf8791daef8f152e992103a1d2b3ff163b00495d85a747eb3a445eb1947e714de0e82dc0094c74f9c0'
